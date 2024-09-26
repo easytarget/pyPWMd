f8914d4ab47e91ae03726dfabc31c5a3351b268b
Owen <owen@iris.easytarget.org>
2024-09-27 01:24:34 +0200

    allows setting

--- pyPWMd.py
+++ pyPWMd.py
@@ -17,7 +17,6 @@ class pypwm_server:
     Screens are 'slid' into place to provide a pleasing animation effect
     A screensaver can be invoked to blank or invert the display as the user wishes
     '''
-    polarities = {'normal':0, 'inversed':1}
 
     def __init__(self, socket=sock, logfile=None):
         '''
@@ -29,6 +28,7 @@ class pypwm_server:
         self.logfile = logfile
         self.sysbase  = '/sys/class/pwm'
         self.chipbase = 'pwmchip'
+        self.polarities = ['normal','inversed']
 
         self._log('scanning for pwm timers')
         chips = self._chipscan()
@@ -59,13 +59,13 @@ class pypwm_server:
     def _gettimer(self,node):
         with open(node + '/enable','r') as f:
            enable = int(f.read())
-        with open(node + '/polarity','r') as f:
-           polarity = self.polarities[f.read().strip()]
         with open(node + '/period','r') as f:
            period = int(f.read())
         with open(node + '/duty_cycle','r') as f:
            duty = int(f.read())
-        return enable, polarity, period, duty
+        with open(node + '/polarity','r') as f:
+           polarity = self.polarities.index(f.read().strip())
+        return enable, period, duty, polarity
 
     def states(self):
         pwms = {}
@@ -86,25 +86,48 @@ class pypwm_server:
         node = '{}/{}{}/pwm{}'.format(self.sysbase,
             self.chipbase, chip, timer)
         if not path.exists(node):
-            self._log('error: attempt to set unexported timer {}'.format(node))
             return None
         return self._gettimer(node)
 
-    def set(self, chip, timer, enabled, priority, period, duty):
+    def set(self, chip, timer, enable=None, period=None, duty=None, polarity=None):
+        def setprop(n, p, v):
+            try:
+                with open(n + '/' + p, 'w') as export:
+                    export.write(str(v))
+            except (FileNotFoundError, OSError) as e:
+                self._log('Cannot set {}/{} :: {}'.format(n, p, repr(e)))
+                return False
+            return True
+
         node = '{}/{}{}/pwm{}'.format(self.sysbase, self.chipbase, chip, timer)
         if not path.exists(node):
             self._log('error: attempt to set unexported timer {}'.format(node))
             return False
         state = self._gettimer(node)
-        if state[0]:  # enabled
-            print('enabled')
+        print(state)
+        if period is not None:
+            # min and max values?
+            # set duty to zero first?
+            if state[3] != period:
+                setprop(node, 'period', period)
+        if duty is not None:
+            #duty = min(period,duty), use period from state if period is None
+            if state[3] != duty:
+                setprop(node, 'duty_cycle', duty)
+        if polarity is not None:
+            if state[3] != polarity:
+                setprop(node, 'polarity', self.polarities[polarity])
+        if enable is not None:
+            if state[0] != enable:
+                setprop(node, 'enable', enable)
+        print(self._gettimer(node))
 
     def open(self, chip, timer):
         node = '{}/{}{}'.format(self.sysbase, self.chipbase, chip)
         if path.exists(node + '/pwm' + str(timer)):
             return True
         try:
-            with open(node + '/export','w') as export:
+            with open(node + '/export', 'w') as export:
                 export.write(str(timer))
         except (FileNotFoundError, OSError) as e:
             self._log('Cannot access {}/export :: {}'.format(node, repr(e)))
@@ -119,7 +142,7 @@ class pypwm_server:
         if not path.exists(node + '/pwm' + str(timer)):
             return True
         try:
-            with open(node + '/unexport','w') as unexport:
+            with open(node + '/unexport', 'w') as unexport:
                 unexport.write(str(timer))
         except (FileNotFoundError, OSError) as e:
             self._log('Cannot access {}/unexport :: {}'.format(node, repr(e)))
